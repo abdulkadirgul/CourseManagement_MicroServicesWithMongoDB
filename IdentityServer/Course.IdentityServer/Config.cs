aafe6ee4474b71e6fcda6f2d263744e318327d56
abdulkadirgul <abdulkadirgull1@gmail.com>
2024-08-21 23:16:55 +0300

    Identity resource and Resource owner credential configurated,Access token and refresh token settings added for new Client.

--- IdentityServer/Course.IdentityServer/Config.cs
+++ IdentityServer/Course.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Course.IdentityServer
@@ -20,6 +21,15 @@ namespace Course.IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
+                       new IdentityResources.Email(),
+                       new IdentityResources.OpenId(),
+                       new IdentityResources.Profile(),
+                       new IdentityResource(){
+                                    Name="roles",
+                                    DisplayName="Roles",
+                                    Description="Kullanıcı Rolleri",
+                                    UserClaims= new[]{"role"} 
+                       }
                    };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -40,6 +50,25 @@ namespace Course.IdentityServer
                     ClientSecrets ={new Secret("secret".Sha256()) },
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes={ "catalog_fullpermission", "photo_sctock_fullpermission",IdentityServerConstants.LocalApi.ScopeName }
+
+                },
+                 new Client
+                {
+                    ClientName = "Asp.Net Core MVC",
+                    ClientId ="WebMvcClientForUser",
+                    ClientSecrets ={new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes={
+                     IdentityServerConstants.StandardScopes.Email,
+                     IdentityServerConstants.StandardScopes.OpenId,
+                     IdentityServerConstants.StandardScopes.Profile,
+                     IdentityServerConstants.StandardScopes.OfflineAccess,
+                     "roles"
+                     },
+                    AccessTokenLifetime=1*60*60,
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime= (int)(DateTime.Now.AddDays(60)- DateTime.Now).TotalSeconds,
+                    RefreshTokenUsage =TokenUsage.ReUse
 
                 }
             };
