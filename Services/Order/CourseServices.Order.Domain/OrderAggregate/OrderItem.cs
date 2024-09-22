bdfb33ba38484983cca4d9d41f88abb59a14a4dd
abdulkadirgul <abdulkadirgull1@gmail.com>
2024-09-22 20:21:58 +0300

    Order Microservice solution added to project with Domain Driven Design  pattern layers.

---
+++ Services/Order/CourseServices.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseServices.Order.Domain.OrderAggregate
+{
+    public class OrderItem
+    {
+        public string ProductId { get;private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public decimal Price { get; private set; }
+
+        public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+    }
+    
+}
